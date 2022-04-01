f6df66aed090af722254da2178a955059e348607
Freddy Román <me@freddy.mx>
2022-03-31 22:38:47 -0700

    more helpers

--- omegaup/validator/parsing.py
+++ omegaup/validator/parsing.py
@@ -4,19 +4,24 @@
 
 import parsy as p   # type: ignore
 
-from typing import Union, Any
+from typing import Any, Optional
 
-space = p.regex(" ").desc("space")
-eol = p.regex("\n").desc("eol")
+space = p.string(" ").desc("space")
+eol = p.string("\n").desc("eol")
 integer = p.regex("-?[0-9]+").map(int).desc("integer")
 
 
-def boundInteger(min: int, max: int) -> Any:
+def boundInteger(min: Optional[int], max: Optional[int]) -> Any:
     @p.generate(f"integer between {min} and {max}")  # type: ignore
-    def parser() -> Union[int, Any]:
+    def parser() -> Any:
         num = yield integer
-        if not min <= num <= max:
+        if min and not min <= num:
+            return p.fail('out of bounds')
+        if max and not num <= max:
             return p.fail('out of bounds')
         return num
 
     return parser
+
+
+natural = boundInteger(0, None)
