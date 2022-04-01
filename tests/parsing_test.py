61645b5ea13a6f5aeff41600918ada49922ecc8d
Freddy Román <me@freddy.mx>
2022-03-31 22:44:34 -0700

    better tests

--- tests/parsing_test.py
+++ tests/parsing_test.py
@@ -3,6 +3,7 @@
 """Test omegaup.validator.validatortest."""
 
 import unittest
+import parsy as p  # type: ignore
 
 import omegaup.validator.parsing as oup
 
@@ -11,7 +12,10 @@ class TestValidatorParsing(unittest.TestCase):
     """Test omegaup.validator.parsing."""
     def test(self) -> None:
         """Basic test."""
-        self.assertEqual(oup.integer.parse('1234'), 1234)
+        self.assertEqual(oup.integer.parse('-1234'), -1234)
+        self.assertEqual(oup.natural.parse('1234'), 1234)
+        self.assertRaises(p.ParseError,
+                          lambda: oup.boundInteger(0, 1).parse("2"))
 
 
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
